c3ece1af04e45edbb7618403b71e04d2c89149ed
Steven Rosario <strosariogarcia@gmail.com>
2022-08-29 23:48:42 -0400

    commit tree da6423d3368e7ab8534f54441f58ae1e9f0b19a5

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = names => {
+    return [names[0], names[1]]; // i feel like there is another way to obtain the first two names without this static search
+}
+
+const returnLastTwoDrivers = names => {
+    const lastTwoNames = names.length - 1;
+    return [names[lastTwoNames-1], names[lastTwoNames]]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (num) => {
+    return function(fare){
+        return fare * num;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (arrOfNames, whosName) => {
+    return (whosName(arrOfNames));
+}
